1bc1a0f9181f9503930a61e5f98ca2c63c0825b6
Rayaninha <salesrayane11@gmail.com>
2024-08-02 11:30:45 -0300

    feat: get hello world

---
+++ src/http/routes/get-hello-world.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+
+export async function getHelloWorld(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/hello-world",
+    {
+      schema: {
+        tags: ["wello-world"],
+        summary: "Simple route",
+        response: {
+          200: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async () => {
+      return {
+        message: "Wello World",
+      };
+    },
+  );
+}
